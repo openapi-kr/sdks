5f5f5ee75f066d35e20b441c86f93bd77d29bc04
hahnlee <hanlee.dev@gmail.com>
2023-03-19 11:12:42 +0000

    feat(mois/addrlink): 주소기반 산업서비스 API 추가 (#3)

---
+++ packages/mois-addrlink/README.md
@@ -0,0 +1,8 @@
+# @openapi-kr/mois-addrlink
+OpenAPI client for @openapi-kr/mois-addrlink.
+정의 파일은 https://github.com/openapi-kr/specifications 에서 확인할 수 있습니다.
+
+# 설치
+```
+npm install @openapi-kr/mois-addrlink --save
+```
---
+++ packages/mois-addrlink/src/models/index.ts
@@ -0,0 +1,15 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AddrCoordResponse';
+export * from './AddrCoordResponseResults';
+export * from './AddrDetailResponse';
+export * from './AddrDetailResponseResults';
+export * from './AddrEngResponse';
+export * from './AddrEngResponseResults';
+export * from './AddrLinkResponse';
+export * from './AddrLinkResponseResults';
+export * from './Address';
+export * from './Common';
+export * from './Coord';
+export * from './DetailAddress';
+export * from './EnglishAddress';
